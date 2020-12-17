7c05b700f5a8a5f67730a7fb2af19c5654c64345
djtepas <djtepas@gmail.com>
2020-12-17 20:00:47 +0000

    Through to Lesson *. Need to learn about Toggle Content with State.

---
+++ src/posts/PostList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PostData from '../data/posts.json'
+import PostDetail from './PostDetail'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.handleDataCallback = this.handleDataCallback.bind(this)
+    }
+    handleDataCallback(txtMsg){
+        alert(txtMsg)
+        console.log(this)
+    }
+
+    render () {
+        
+        return (
+            <div>
+                <h1>Hello There</h1>
+                {PostData.map((item, index) => {
+                    return <PostDetail 
+                        post={item} 
+                        key={`post-list-key ${index}`} 
+                        dataCallback={this.handleDataCallback} />
+                })}
+            </div>
+        )
+    }
+}
+
+export default App;
